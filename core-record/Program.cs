b11c62d5adc1635277dd508c5fe1ad36b0a3bbe6
Gilad <gilad.hanya@gmail.com>
2021-10-29 10:22:28 +0300

    Add base code

---
+++ core-record/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace core_record
+{
+    //https://www.youtube.com/watch?v=oSQDZ7GpKwg
+    //Read only
+    //Generate extra code in assembly that implements:
+    //ToString
+    //{ get; init; }
+    //implement comperison
+    //override GetHashCode
+    //Copy
+    //Clone
+    //Deconstruct to Tupple.
+    public class Record1
+	{
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+    public record Record(string FirstName, string LastName);
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var data = new Record("Moshe", "Cohen");
+            var data2 = data with { LastName = "Levy" };
+            Console.WriteLine(data.ToString());
+        }
+    }
+}
